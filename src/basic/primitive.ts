0e63197f55e467002e5770501414bd4987e26044
sou7322 <skoike322@gmail.com>
2022-04-24 17:07:42 +0900

    [Fix] リント修正

--- src/basic/primitive.ts
+++ src/basic/primitive.ts
@@ -1,17 +1,17 @@
 export default function primitiveSample() {
-  let name: string = 'Amy';
+  const name: string = 'Amy';
 
   // string型の変数に数値を代入することはできない
   // name = 123; => error
 
   console.log('name:', typeof name, name);
 
-  let age: number = 14;
+  const age: number = 14;
   console.log('age:', typeof age, age);
 
-  let isFemale: boolean = true;
+  const isFemale: boolean = true;
   console.log('isFemale:', typeof isFemale, isFemale);
 
-  let isOver20: boolean = age >= 20;
+  const isOver20: boolean = age >= 20;
   console.log('isOver20:', typeof isOver20, isOver20);
 }
