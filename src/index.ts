0f182286c6e9be885c8734d4356af56de200910a
sou7322 <skoike322@gmail.com>
2022-04-25 17:23:59 +0900

    [Add] オプション・デフォルト・レストパラメータ、呼び出しシグネチャ学習用のファイルを作成

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-/* 関数の型定義 */
+/* 関数の型定義 
 
 import { logMessage } from './function/basic';
 logMessage('hello');
 
-import { alwaysThrowError } from './function/basic';
-alwaysThrowError('Throw Error!!!');
+// import { alwaysThrowError } from './function/basic';
+// alwaysThrowError('Throw Error!!!');
 
-/* ここまで */
+import { isUserLoggedIn } from './function/parameters';
+isUserLoggedIn(123);
+isUserLoggedIn(123, 'Amy');
+isUserLoggedIn(321);
+
+import { isUserLoggedIn2 } from './function/parameters';
+isUserLoggedIn2(123, 'Amy');
+isUserLoggedIn2(123);
+
+import { sumProductsPrice } from './function/parameters';
+const sum = sumProductsPrice(100, 200, 300, 400, 500);
+console.log('sumProductsPrice:', sum);
+
+import { logName } from './function/parameters';
+logName('Amy');
+
+import { fullLogName } from './function/parameters';
+fullLogName('Amy');
+ここまで */
 
 /* 基本の型定義
 
