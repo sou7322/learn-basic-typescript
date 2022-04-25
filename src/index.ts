9d04d5b7b50057f073f559d9c07bf4eaefdcbc4c
sou7322 <skoike322@gmail.com>
2022-04-25 14:38:00 +0900

    [Add] void, never型学習用のファイルを作成

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
+/* 関数の型定義 */
+
+import { logMessage } from './function/basic';
+logMessage('hello');
+
+import { alwaysThrowError } from './function/basic';
+alwaysThrowError('Throw Error!!!');
+
+/* ここまで */
+
+/* 基本の型定義
+
 import { unknownSample, anySample, notExistSample, primitiveSample } from './basic/index';
 
 primitiveSample();
 notExistSample();
 anySample();
 unknownSample();
+
+ここまで */
 
 // import World from './world';
 // const root = document.getElementById('root');
