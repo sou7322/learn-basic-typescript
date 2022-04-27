6bcf081c28f28a5f1a6db3429916607c7ba7349c
sou7322 <skoike322@gmail.com>
2022-04-27 19:24:04 +0900

    [Fix] リント修正

--- src/array/tuple.ts
+++ src/array/tuple.ts
@@ -1,7 +1,7 @@
 export default function tupleSample() {
   // 厳格な配列 = タプル
   // * 配列が持つ要素の数、各要素の型を定義することができる
-  let tuple1: [string, number] = ['string', 1];
+  const tuple1: [string, number] = ['string', 1];
 
   // tuple1 = ['string', 1, 'string2'];
   // tuple1 = [1, 'string'];
